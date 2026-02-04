a4ca54358f38df6cdaabdc889a8fc4a131e36b73
Krzysztof-StaCho <krzysztof.stachowaniec@gmail.com>
2026-02-04 09:56:44 +0100

    Adding custom fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { ColorsTheme, ContainerStyle } from './utils/BaseStyle';
 import { CardShelf } from './components/molecules/CardShelf';
 import { ScrollView, View } from 'react-native';
 import { PageHeader } from './components/atoms/PageHeader';
 import { PageFooter } from './components/atoms/PageFooter';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [loaded, error] = useFonts({
+    Bangers: require("./assets/fonts/Bangers.ttf"),
+    Macondo: require("./assets/fonts/Macondo.ttf"),
+    Zalando: require("./assets/fonts/Zalando.ttf")
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
   return (
     <SafeAreaView style={ContainerStyle.OuterContainerStyle}>
       <View style={ContainerStyle.InnerContainerStyle}>
