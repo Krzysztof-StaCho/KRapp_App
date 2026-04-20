df02e3075bbaebe00ca4bf8c744727e557ec75ec
Krzysztof-StaCho <krzysztof.stachowaniec@gmail.com>
2026-04-20 13:45:10 +0200

    feat: restructure FakeData; define types for Raport and initialize data structure

--- data/FakeData.ts
+++ data/FakeData.ts
@@ -1,32 +1,101 @@
-const RaportList = [
-    { id: 1, title: "Kierowcy Zamówienie" },
-    { id: 2, title: "Szafka" },
-    { id: 3, title: "Napoje" }
-];
+type RaportHeaderType = {
+    id: number,
+    title: string
+};
 
-const RaportOverview_01 = {};
+type RaportDataType = {
+    id: number,
+    name: string,
+    amount: number,
+    quantity: "paczka" | "sztuk"
+};
 
-const RaportData_01 = [
+type RaportType = {
+    id: number,
+    title: string,
+    data: RaportDataType[]
+};
+
+export const InitData: RaportType[] = [
     {
         id: 1,
-        name: "Kartony pizza 32 cm",
-        amount: 1,
-        quantity: "paczka"
+        title: "Kierowcy Zamówienie",
+        data: [
+            {
+                id: 1,
+                name: "Kartony pizza 32 cm",
+                amount: 1,
+                quantity: "paczka"
+            },
+            {
+                id: 2,
+                name: "Kartony pizza 45 cm",
+                amount: 5,
+                quantity: "paczka"
+            },
+            {
+                id: 3,
+                name: "Pudełka - Burger wołowy",
+                amount: 0.9,
+                quantity: "paczka"
+            }
+        ]
     },
     {
         id: 2,
-        name: "Kartony pizza 45 cm",
-        amount: 4,
-        quantity: "paczka",
+        title: "Szafka",
+        data: [
+            {
+                id: 1,
+                name: "Mikrofibra",
+                amount: 5,
+                quantity: "sztuk"
+            },
+            {
+                id: 2,
+                name: "Rękawiczki M",
+                amount: 1,
+                quantity: "paczka"
+            },
+            {
+                id: 3,
+                name: "Reklamówki 1 zestaw",
+                amount: 3,
+                quantity: "paczka"
+            }
+        ]
     },
     {
         id: 3,
-        name: "Pudełka - Burger Wołowy",
-        amount: 0.9,
-        quantity: "paczka"
+        title: "Napoje",
+        data: [
+            {
+                id: 1,
+                name: "Pepsi 0.85L",
+                amount: 4,
+                quantity: "paczka"
+            }
+        ]
     }
 ];
 
-export const GetRaports = () => RaportList;
-export const GetRaportOverview = (id: number) => RaportOverview_01;
-export const GetRaportDetails = (id: number) => RaportData_01;
+export function GetRaports(): RaportHeaderType[] {
+    const getRaportHeaders = (item: RaportType): RaportHeaderType => {
+        return {
+            id: item.id,
+            title: item.title
+        };
+    };
+
+    return InitData.map(getRaportHeaders);
+};
+
+export function GetRaportOverview(id: number) {
+    return {};
+}
+
+export function GetRaportDetails(id: number): RaportDataType[] {
+    const target = InitData.find((item) => item.id === id);
+    if (!target) throw Error();
+    return target.data;
+};
