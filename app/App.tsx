4b03dd24e6ff5670a7fee1e0e614afcf6ca0b86d
Krzysztof-StaCho <krzysztof.stachowaniec@gmail.com>
2026-04-27 20:52:43 +0200

    Add typography styles and integrate into App component

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
+import { Typography } from '@/theme/typography';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
     return (
         <NavigationContainer>
             <SafeAreaView>
+                <Text style={Typography['H1']}>Preview Title</Text>
+                <Text style={Typography['Body']}>Sample body text. Not long but enough to check if works just fine.</Text>
                 <StatusBar style='light' />
             </SafeAreaView>
         </NavigationContainer>
